c056d39128b735601788eaa1ea1422cf1ae576bc
ococa <wangchao.personal@gmail.com>
2019-12-29 18:57:39 +0800

    using parcel compiles

--- button.js
+++
@@ -1,3 +0,0 @@
-Vue.component('g-button', {
-    template: `<button class="g-button">h12222222222222222</button>`
-})
---
+++ src/button.vue
@@ -0,0 +1,26 @@
+
+<template>
+    <button class="g-button">h12222222222222222</button>
+</template>
+<srcipt>
+    export default {}
+</srcipt>
+<style lang="scss">
+    .g-button {
+        font-size: var(--font-size);
+        height: var(--button-height);
+        padding: 0 1em;
+        border-radius: var(--button-border-radius);
+        border: 1px solid var(--border-color);
+        background: var(--button-bg);
+        &:hover {
+            border-color: var(--border-color-horver);
+        }
+        &:active {
+            background-color: var(--button-active-bg);
+        }
+        &:focus {
+            outline: none;
+        }
+    }
+</style>
